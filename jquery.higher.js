35903cb964bd4103d9565aa467e93794b73b65ab
maechabin <mae@chab.in>
2014-11-08 03:40:41 +0900

    fix options function

--- jquery.higher.js
+++ jquery.higher.js
@@ -1,16 +1,20 @@
 /*!
- * jquery.higher.js v0.5
+ * jquery.higher.js v0.6
  * Auther @maechabin
  * Licensed under mit license
  */
 
-
 ;(function ($, window, document, undefined) {
 
-    var Plugin = function (element) {
+    var Plugin = function (element, options) {
 
         this.element = element;
         this.$element = $(element);
+        this.config;
+        this.defaults = {
+            attributes: [element]
+        };
+        this.options = options;
         this.timer = null;
 
     };
@@ -18,89 +22,96 @@
 
     Plugin.prototype.equalBoxHeight = function (num1, num2) {
 
-        var elm = this.$element.children();
+        var elm = this.config.attributes;
 
-        elm.each(function (i) {
+        for (var i = 0, l = elm.length; i < l; i++) {
 
-            var height;
-            var $this = $(this);
+            var e = (this.options) ? $(elm[i]) : $(elm[i]).children() ;
 
-            if (i === num1) {
+            e.each(function (j) {
 
-                height = $this.height();
+                var height;
+                var $this = $(this);
 
-                for (var n = num1 + 1; n <= num1 + num2 - 1; n++) {
+                if (j === num1) {
 
-                    if (height < elm.eq(n).height()) {
+                    height = $this.height();
 
-                        height = elm.eq(n).height();
+                    for (var n = num1 + 1; n <= num1 + num2 - 1; n++) {
+
+                        if (height < e.eq(n).height()) {
+
+                            height = e.eq(n).height();
+
+                        }
+
+                    }
+
+                    for (var n = num1; n <= num1 + num2 - 1; n++) {
+
+                        e.eq(n).css("height", height + "px");
 
                     }
 
                 }
 
-                for (var n = num1; n <= num1 + num2 - 1; n++) {
+            });
 
-                    elm.eq(n).css("height", height + "px");
-
-                }
-
-           }
-
-        });
+        }
 
     };
 
 
     Plugin.prototype.getHeight = function () {
 
-        var elm = this.$element.children();
+        var elm = this.config.attributes;
 
-        var start_num;
-        var line_flag = 0;
-        var l = elm.length;
+        for (var i = 0, l = elm.length; i < l; i++) {
 
-        for (var i = 0; i < l; i++) {
+            var e = (this.options) ? $(elm[i]) : $(elm[i]).children() ;
+            var start_num;
+            var line_flag = 0;
+            var len = e.length;
 
-            var box1 = elm.eq(i);
-            var top1 = box1.offset().top;
+            for (var j = 0; j < len; j++) {
 
-            if (i < l - 1) {
+                var box1 = e.eq(j);
+                var top1 = box1.offset().top;
 
-                var box2 = elm.eq(i + 1);
-                var top2 = box2.offset().top;
+                if (j < len - 1) {
 
-            }
+                    var box2 = e.eq(j + 1);
+                    var top2 = box2.offset().top;
 
-            if (line_flag === 0) {
-
-                var box_array = [];
-                start_num = i;
-
-            }
-
-            if (top1 == top2) {
-
-                box_array.push(box1)
-                line_flag = 1;
-
-            } else {
-
-                box_array.push(box1);
-                this.equalBoxHeight(start_num, box_array.length);
-
-                if (box2) {
-                    box2.css("clear", "both");
                 }
 
-                start_num = "";
-                line_flag = 0;
+                if (line_flag === 0) {
+
+                    var box_array = [];
+                    start_num = j;
+
+                }
+
+                if (top1 == top2) {
+
+                    box_array.push(box1)
+                    line_flag = 1;
+
+                } else {
+
+                    box_array.push(box1);
+                    this.equalBoxHeight(start_num, box_array.length);
+
+                    start_num = "";
+                    line_flag = 0;
+
+                }
 
             }
 
-        }
+            this.equalBoxHeight(start_num, box_array.length);
 
-        this.equalBoxHeight(start_num, box_array.length);
+        }
 
     };
 
@@ -126,18 +137,26 @@
 
         this.timer = setTimeout(function () {
 
-            var elm = _this.$element.children();
+            var elm = _this.config.attributes;
 
-            elm.each(function () {
+            for (var i = 0, l = elm.length; i < l; i++) {
 
-                $(this).css({
-                    "clear": "none",
-                    "height": "auto"
+                var e = (_this.options) ? $(elm[i]) : $(elm[i]).children() ;
+
+                e.each(function () {
+                
+                    var $this = $(this);
+
+                    $this.css({
+                        "clear": "none",
+                        "height": "auto"
+                    });
+
                 });
 
-            });
+                _this.getHeight();
 
-            _this.getHeight();
+            }
 
         }, 200);
 
@@ -145,23 +164,27 @@
 
 
     Plugin.prototype.init = function () {
+        
+        this.config = $.extend({}, this.defaults, this.options);
 
         this.getHeight();
         this.getResize();
+
+        //console.log(this.config.attributes);
 
         return this;
 
     };
 
 
-    $.fn.higher = function () {
+    $.fn.higher = function (options) {
 
         return this.each(function () {
 
-            new Plugin(this).init();
+            new Plugin(this, options).init();
 
         });
 
     };
 
-})(jQuery, window, document);
+} (jQuery, window, document));
